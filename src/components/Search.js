d42e1d83b030f92efc45242b8d947f1e43234e48
Prince-Arinze <princendu98@gmail.com>
2021-06-03 23:59:34 +0100

    made the searchbox responsive

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,15 +5,12 @@ const Search = () => {
     const inputEl = useRef("")
     const { searchTerm, getSearchTerm } = useContext(DataContext)
     return(
-        <section className="section newsletter" id="subscribe">
-            <div className="newsletter__container bd-grid">
-                <div className="newsletter__subscribe">
-                
-                    <form action="" className="newsletter__form">
-                        <input type="text" ref={inputEl} value={searchTerm} className="newsletter__input" placeholder="Search Products" onChange={() => getSearchTerm(inputEl.current.value)}/>
-                        <span className="button-seach"><FaSearch className="search-icon"/></span>
+        <section>
+            <div className="bd-grid">
+                    <form className="search__form">
+                        <input type="text" ref={inputEl} value={searchTerm} className="search__input" placeholder="Search Products" onChange={() => getSearchTerm(inputEl.current.value)}/>
+                        <FaSearch className="search-icon"/>
                     </form>
-                </div>
             </div>
         </section>
     )
